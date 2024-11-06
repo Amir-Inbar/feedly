02103abba03712edc703b91531996b2468142b05
amirinbarcode@gmail.com <amirinbarcode@gmail.com>
2024-11-06 22:15:26 +0200

    feat: adds motion for liking

--- src/components/PostCard/PostCardActions.tsx
+++ src/components/PostCard/PostCardActions.tsx
@@ -1,6 +1,7 @@
 import {FC} from 'react';
 import {FeedlyIcon} from "../../shared/IconifyConfig/FeedlyIcon.tsx";
 import {Post} from "../../types/feed.ts";
+import {motion} from 'framer-motion';
 
 interface PostCardActionsProps {
     post: Post;
@@ -9,20 +10,28 @@ interface PostCardActionsProps {
 }
 
 export const PostCardActions: FC<PostCardActionsProps> = ({post, onLike}) => {
-
     const likeIcon = post.didLike ? 'customLiked' : 'customLike';
     const likeClassName = post.didLike ? 'text-post-like-active' : 'text-post-like-inactive';
+
     return (
         <div className="flex justify-evenly items-center p-6">
-            <div className="flex flex-row cursor-pointer" onClick={() => onLike(post.id, !post.didLike)}>
+            <motion.div
+                className="flex flex-row cursor-pointer"
+                onClick={() => onLike(post.id, !post.didLike)}
+                whileTap={{
+                    scale: 0.9,
+                }}
+                transition={{type: "spring", stiffness: 300, damping: 10}}
+            >
                 <FeedlyIcon icon={likeIcon}/>
                 <span className={`${likeClassName} ml-2`}>
-                            Like
-                        </span>
-            </div>
+                    Like
+                </span>
+            </motion.div>
             <div className="flex items-center cursor-pointer text-post-like-inactive">
-                <FeedlyIcon icon='customComment'/>
-                <span className='ml-2'>Comment</span>
+                <FeedlyIcon icon="customComment"/>
+                <span className="ml-2">Comment</span>
             </div>
-        </div>)
-}
+        </div>
+    );
+};
