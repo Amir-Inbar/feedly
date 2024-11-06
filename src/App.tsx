6ada23f3d3d1467c35e31849e4533826c34cf821
amirinbarcode@gmail.com <amirinbarcode@gmail.com>
2024-11-06 21:28:38 +0200

    feat: complete post

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
         <Provider store={store}>
             <div className="min-h-screen bg-gray-50">
                 <Navbar/>
-                <main className="pt-16 md:pt-20 max-w-[1120px] mx-auto">
+                <main className="pt-16 md:pt-20 max-w-[680px] mx-auto">
                     <Feed/>
                 </main>
             </div>
--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {motion} from 'framer-motion';
 import {Post} from '../../types/feed.ts';
 import dayjs from 'dayjs';
@@ -10,15 +10,11 @@ dayjs.extend(relativeTime);
 
 interface PostCardProps {
     post: Post;
-    onLike: (postId: string) => void;
+
+    onLike(postId: string, isLiked: boolean): void;
 }
 
 export const PostCard: React.FC<PostCardProps> = ({post, onLike}) => {
-
-    useEffect(() => {
-        fetch(`https://backend.tedooo.com/?itemId=${post.id}`).catch(console.error);
-    }, [post.id]);
-
     return (
         <motion.article
             initial={{opacity: 0, y: 20}}
@@ -48,7 +44,7 @@ export const PostCard: React.FC<PostCardProps> = ({post, onLike}) => {
                 </div>
                 {post.images.length > 0 && <ImageGallery images={post.images}/>}
                 <div className="flex justify-between items-center px-[25px] py-4 text-sm text-post-like-count">
-                    <div className="flex items-center" onClick={() => onLike(post.id)}>
+                    <div className="flex items-center">
                         <div
                             className="flex items-center justify-center w-[20px] h-[20px] bg-gradient-to-b from-[#96DCD0] to-[#208171] rounded-full pr-0.5 mr-2">
                             <FeedlyIcon icon='customLikeCount'/>
@@ -63,14 +59,15 @@ export const PostCard: React.FC<PostCardProps> = ({post, onLike}) => {
                 </div>
                 <hr className="border-t border-gray-300 mx-[25px]"/>
                 <div className="flex justify-evenly items-center p-6">
-                    <div className="flex flex-row cursor-pointer " onClick={() => onLike(post.id)}>
-                        <FeedlyIcon icon={post.didLike ? 'customLiked' : 'customLike'} className='mr-1'/>
-                        <span className={post.didLike ? 'text-post-like-active' : 'text-post-like-inactive'}>
+                    <div className="flex flex-row cursor-pointer" onClick={() => onLike(post.id, !post.didLike)}>
+                        <FeedlyIcon icon={post.didLike ? 'customLiked' : 'customLike'}/>
+                        <span className={`${post.didLike ? 'text-post-like-active' : 'text-post-like-inactive'} ml-2`}>
                             Like
                         </span>
                     </div>
                     <div className="flex items-center cursor-pointer text-post-like-inactive">
-                        <span>Comment</span>
+                        <FeedlyIcon icon='customComment'/>
+                        <span className='ml-2'>Comment</span>
                     </div>
                 </div>
             </section>
